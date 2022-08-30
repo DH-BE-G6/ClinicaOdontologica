20b80fa1cdfe10799b2230181b21e61df0c3471e
Matheus Ferreira <matheusrbferreira@gmail.com>
2022-08-30 20:47:17 -0300

    [Adição] Estrutura inicial de pacotes e entidades
    Entidades:
    - Paciente
    - Dentista
    - Endereco
    
    Dependências adicionadas:
    - JUnit
    - H2 Database

---
+++ src/main/java/com/digitalhouse/clinicaodontologicag6/entity/DentistaEntity.java
@@ -0,0 +1,42 @@
+package com.digitalhouse.clinicaodontologicag6.entity;
+
+public class DentistaEntity {
+
+    private int id;
+    private String nome;
+    private String sobrenome;
+    private String matricula;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+}
---
+++ src/main/java/com/digitalhouse/clinicaodontologicag6/entity/PacienteEntity.java
@@ -0,0 +1,61 @@
+package com.digitalhouse.clinicaodontologicag6.entity;
+
+import java.util.Date;
+
+public class PacienteEntity {
+
+    private int id;
+    private String nome;
+    private String sobrenome;
+    private String documentoIdentificacao;
+    private String endereco;
+    private Date dataAlta;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getDocumentoIdentificacao() {
+        return documentoIdentificacao;
+    }
+
+    public void setDocumentoIdentificacao(String documentoIdentificacao) {
+        this.documentoIdentificacao = documentoIdentificacao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Date getDataAlta() {
+        return dataAlta;
+    }
+
+    public void setDataAlta(Date dataAlta) {
+        this.dataAlta = dataAlta;
+    }
+}
