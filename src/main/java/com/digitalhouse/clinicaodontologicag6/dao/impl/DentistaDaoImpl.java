d6db36348892835d62e037a7858b7245ba3ee212
Marta Pederiva <80361071+mpederiva@users.noreply.github.com>
2022-08-31 22:50:52 -0300

    Update PacienteDaoImpl + Upload PacienteDaoImpl
    - Update PacienteDaoImpl: Erro logger
    - Update PacienteDaoImpl: Implementar classes DentistaDaoImpl
    - Update PacienteDaoImpl: DentistaEntity em Dentista, na database estava diferente (só Entity)
    - Update PacienteDaoImpl: %s a mais Na linha24
    - Update PacienteDaoImpl: Criar connection emm get e update
    - Upload PacienteDaoImpl: fazer tudo

--- src/main/java/com/digitalhouse/clinicaodontologicag6/dao/impl/DentistaDaoImpl.java
+++ src/main/java/com/digitalhouse/clinicaodontologicag6/dao/impl/DentistaDaoImpl.java
@@ -5,13 +5,11 @@ import com.digitalhouse.clinicaodontologicag6.dao.IDao;
 import com.digitalhouse.clinicaodontologicag6.entity.DentistaEntity;
 import org.apache.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DentistaDaoImpl implements IDao<DentistaEntity> {
     private ConfigJDBC configJDBC;
-    final static Logger log = Logger.getLogger(DentistaDaoImpl.class);
+    final static Logger logger = Logger.getLogger(DentistaDaoImpl.class);
     private Connection connection = configJDBC.conectarBD();
 
     public DentistaDaoImpl(ConfigJDBC configJDBC) {
@@ -21,7 +19,7 @@ public class DentistaDaoImpl implements IDao<DentistaEntity> {
     public DentistaEntity create(DentistaEntity dentistaEntity) {
         Connection connection = configJDBC.conectarBD();
         Statement statement = null;
-        String query = String.format("INSERT INTO dentistaEntity(nome,sobrenome,matricula) VALUES('%s','%s','%s','%s')",
+        String query = String.format("INSERT INTO dentista(nome,sobrenome,matricula) VALUES('%s','%s','%s')",
                 dentistaEntity.getNome(), dentistaEntity.getSobrenome(), dentistaEntity.getMatricula());
         try {
             statement = connection.createStatement();
@@ -40,7 +38,7 @@ public class DentistaDaoImpl implements IDao<DentistaEntity> {
 
     public void delete (int id){
         Statement statement = null;
-        String query = String.format("DELETE FROM dentistaEntity WHERE id = '%s'"), id);
+        String query = String.format("DELETE FROM dentista WHERE id = '%s'", id);
 
         try {
             statement = this.connection.createStatement();
@@ -52,5 +50,49 @@ public class DentistaDaoImpl implements IDao<DentistaEntity> {
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public DentistaEntity get(int id) {
+        Connection connection = configJDBC.conectarBD();
+        String query = "SELECT * FROM dentista WHERE id = ?";
+        DentistaEntity dentista = null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1,id);
+            preparedStatement.execute();
+            ResultSet resultSet = preparedStatement.getResultSet();
+
+            while (resultSet.next()){
+                dentista = new DentistaEntity(resultSet.getInt("id"), resultSet.getString("nome"));
+            }
+        } catch (SQLException ex){
+            ex.printStackTrace();
+        }
+        return dentista;
+
+    }
+
+    @Override
+    public DentistaEntity update(int id) {
+        Connection connection = configJDBC.conectarBD();
+        Statement statement = null;
+        DentistaEntity dentistaEntity = new DentistaEntity();
+        String query = String.format("UPDATE INTO dentista(nome,sobrenome,matricula) VALUES('%s','%s','%s')",
+                dentistaEntity.getNome(), dentistaEntity.getSobrenome(), dentistaEntity.getMatricula(),
+                " where id=%s", id);
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                dentistaEntity.setId(resultSet.getInt(1));
+            }
+            logger.debug("Salvando Dentista no sistema: " + dentistaEntity.toString());
+            connection.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return dentistaEntity;
     }
 }
