b1136b0863f26b3327ba531d2cbf40b0b63da704
Marta Pederiva <80361071+mpederiva@users.noreply.github.com>
2022-09-06 22:59:39 -0300

    upload IEnderecoRepository

---
+++ src/main/java/com/digitalhouse/clinicaodontologicag6/repository/IEnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.digitalhouse.clinicaodontologicag6.repository;
+
+import com.digitalhouse.clinicaodontologicag6.entity.EnderecoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IEnderecoRepository extends JpaRepository<EnderecoEntity, Integer> {
+}
